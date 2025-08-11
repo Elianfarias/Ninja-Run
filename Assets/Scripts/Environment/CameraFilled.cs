b7e1d946c97199ce1dd533741b8a39396f17cfec
Elian <dev.elian.farias@gmail.com>
2025-08-11 14:11:10 -0300

    Fix camera

---
+++ Assets/Scripts/Environment/CameraFilled.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class CameraFilled : MonoBehaviour
+{
+    [SerializeField]
+    float targetWorldWidth = 16f; // tu ancho de diseño
+    void LateUpdate()
+    {
+        var cam = GetComponent<Camera>();
+        cam.orthographicSize = targetWorldWidth / cam.aspect / 2f;
+    }
+}
